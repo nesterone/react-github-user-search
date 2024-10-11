2183bda6e448eea1472a486a5b03c7c8ce0c0ef6
nesterone <igornester@gmail.com>
2024-10-11 16:12:06 +0200

    add card

--- src/UserSearchResults.tsx
+++ src/UserSearchResults.tsx
@@ -1,17 +1,35 @@
 import {useUserSearch} from "./useUserSearch.tsx";
 
-interface UserSearchResults {
-  query: string;
-  perPage?: number;
-  page?: number;
+interface UserSearchResultsProps {
+    query: string;
+    perPage?: number;
+    page?: number;
 }
 
-export function UserSearchResults({ query, perPage, page }: UserSearchResults) {
-  const { data, error } = useUserSearch({ query, perPage, page });
+export function UserSearchResults({query, perPage, page}: UserSearchResultsProps) {
+    const {data, error} = useUserSearch({query, perPage, page});
 
-   if (error) {
-      return <span>{error.message}</span>
-   }
+    if (error) {
+        return <span>{error.message}</span>
+    }
 
-  return JSON.stringify(data, null, 2);
+    return (<>{data?.items.map(item => <UserCard avatar={item.avatar_url} login={item.login}/>)}</>)
+}
+
+
+interface UserCardProps {
+    login: string;
+    avatar: string;
+}
+
+function UserCard({login, avatar}: UserCardProps) {
+    return (<div className="flex flex-1 flex-col p-8">
+        <img className="mx-auto h-32 w-32 flex-shrink-0 rounded-full" src={avatar} alt="User Avatar"/>
+        <h3 className="mt-6 text-sm font-medium text-gray-900">{login}</h3>
+        <dl className="mt-1 flex flex-grow flex-col justify-between">
+            <dt className="sr-only">Type</dt>
+            <dd className="text-sm text-gray-500">type</dd>
+        </dl>
+    </div>)
+
 }
--- src/useUserSearch.tsx
+++ src/useUserSearch.tsx
@@ -1,14 +1,24 @@
-import useSWR from "swr";
-const fetcher = (info: RequestInfo | URL, init?: RequestInit) =>
+import useSWR, {SWRResponse} from "swr";
+
+interface GithubUserSearchResponse {
+    total_count: number,
+    incomplete_results: boolean,
+    items: GithubUserSearchResultItem[];
+}
+interface GithubUserSearchResultItem {
+    login: string;
+    avatar_url: string;
+}
+const fetcher = (info: RequestInfo | URL, init?: RequestInit): Promise<GithubUserSearchResponse> =>
     fetch(info, init).then((res) => res.json());
+
 interface UserSearchHookParams {
     query: string;
     perPage?: number;
     page?: number;
 }
-
-export function useUserSearch({query, perPage = 3, page = 1}: UserSearchHookParams) {
-    return useSWR(
+export function useUserSearch({query, perPage = 3, page = 1}: UserSearchHookParams): SWRResponse<GithubUserSearchResponse> {
+    return useSWR<GithubUserSearchResponse>(
         `https://api.github.com/search/users?q=${query}&per_page=${perPage}&page=${page}`,
         fetcher,
     );
