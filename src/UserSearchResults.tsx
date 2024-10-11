751e5b67b3e19c1dced3e6aacca0fb731107f275
nesterone <igornester@gmail.com>
2024-10-11 15:37:28 +0200

    fix typings

--- src/UserSearchResults.tsx
+++ src/UserSearchResults.tsx
@@ -1,10 +1,31 @@
-export const API_BASE_URL = "https://api.github.com/search/users";
+import useSWR from "swr";
+const fetcher = (info: RequestInfo | URL, init?: RequestInit)  =>
+  fetch(info, init).then((res) => res.json());
+interface UserSearchHookParams {
+  query: string;
+  perPage?: number;
+  page?: number;
+}
+
+function useUserSearch({ query, perPage = 3, page = 1 }: UserSearchHookParams) {
+  return useSWR(
+    `https://api.github.com/search/users?q=${query}&per_page=${perPage}&page=${page}`,
+    fetcher,
+  );
+}
 
 interface UserSearchResults {
   query: string;
-  itemsPerPage: number;
+  perPage?: number;
+  page?: number;
 }
 
-export function UserSearchResults({ query, itemsPerPage }: UserSearchResults) {
-  return `Query ${query} and with ${itemsPerPage}`;
+export function UserSearchResults({ query, perPage, page }: UserSearchResults) {
+  const { data, error } = useUserSearch({ query, perPage, page });
+
+   if (error) {
+      return <span>{error.message}</span>
+   }
+
+  return JSON.stringify(data, null, 2);
 }
