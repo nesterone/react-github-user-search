70a0e11bf823a0ff33981c261e20131345107e81
nesterone <igornester@gmail.com>
2024-10-11 14:34:04 +0200

    add url

--- src/UserSearchResults.tsx
+++ src/UserSearchResults.tsx
@@ -1,11 +1,10 @@
+export const API_BASE_URL = "https://api.github.com/search/users";
+
 interface UserSearchResults {
-    query: string;
-    itemsPerPage: number;
+  query: string;
+  itemsPerPage: number;
 }
 
-export function UserSearchResults({query, itemsPerPage}: UserSearchResults) {
-
-    return `Query ${query} and with ${itemsPerPage}`;
-
-
+export function UserSearchResults({ query, itemsPerPage }: UserSearchResults) {
+  return `Query ${query} and with ${itemsPerPage}`;
 }
